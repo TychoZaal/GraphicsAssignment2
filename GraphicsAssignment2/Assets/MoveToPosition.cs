374fc154b8bc19fe4a34d759576662333515517a
DESKTOP-1C1O5FT\Tycho <TychoZaal@gmail.com>
2019-03-08 19:33:09 +0100

    Setup

---
+++ GraphicsAssignment2/Assets/MoveToPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPosition : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    public Transform targetObject;
+
+    private int current;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            targetObject = target[current];
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+    }
+}
